f88d19b2b20ee3d015b59fdab2b89056421e275e
Cesar Amadori <cesargamadori@gmail.com>
2023-10-23 20:41:29 -0300

    base project

---
+++ hexatodo/src/main/java/com/cga/hexatodo/HexatodoApplication.java
@@ -0,0 +1,13 @@
+package com.cga.hexatodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexatodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexatodoApplication.class, args);
+	}
+
+}
---
+++ hexatodo/src/test/java/com/cga/hexatodo/HexatodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cga.hexatodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexatodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
