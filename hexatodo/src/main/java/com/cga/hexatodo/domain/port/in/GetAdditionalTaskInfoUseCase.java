490037ba59a87068d8b3ec3cda60809bb9b760cc
Cesar Amadori <cesargamadori@gmail.com>
2023-10-24 08:48:44 -0300

    domain layer interface name update

--- hexatodo/src/main/java/com/cga/hexatodo/domain/port/in/GetAdditionalTaskInfoUseCase.java
+++ hexatodo/src/main/java/com/cga/hexatodo/domain/port/in/GetAdditionalTaskInfoUseCase.java
@@ -2,7 +2,7 @@ package com.cga.hexatodo.domain.port.in;
 
 import com.cga.hexatodo.domain.model.AdditionalTaskInfo;
 
-public interface GetAdditionalTaskUserCase {
+public interface GetAdditionalTaskInfoUseCase {
 
     AdditionalTaskInfo getAddittionalTaskInfo(Long id);
 }
