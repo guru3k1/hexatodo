2ffc5aa6238e5e6d7de783b9ef53ba73e414b853
Cesar Amadori <cesargamadori@gmail.com>
2023-10-24 11:23:42 -0300

    Task model fix

--- hexatodo/src/main/java/com/cga/hexatodo/domain/model/Task.java
+++ hexatodo/src/main/java/com/cga/hexatodo/domain/model/Task.java
@@ -8,12 +8,14 @@ public class Task {
     private String title;
     private String description;
     private LocalDateTime creationDate;
+    private boolean completed;
 
-    public Task(Long id, String title, String description, LocalDateTime creationDate) {
+    public Task(Long id, String title, String description, LocalDateTime creationDate, boolean completed) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.creationDate = creationDate;
+        this.completed = completed;
     }
 
     public Long getId() {
@@ -46,5 +48,13 @@ public class Task {
 
     public void setCreationDate(LocalDateTime creationDate) {
         this.creationDate = creationDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 }
