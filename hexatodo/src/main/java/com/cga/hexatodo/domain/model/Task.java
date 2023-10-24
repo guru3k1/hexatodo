6177e41dfbd3d7f88f7077563522c8aa97fcd192
Cesar Amadori <cesargamadori@gmail.com>
2023-10-24 08:23:06 -0300

    models add

---
+++ hexatodo/src/main/java/com/cga/hexatodo/domain/model/Task.java
@@ -0,0 +1,50 @@
+package com.cga.hexatodo.domain.model;
+
+import java.time.LocalDateTime;
+
+public class Task {
+
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime creationDate;
+
+    public Task(Long id, String title, String description, LocalDateTime creationDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.creationDate = creationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+}
